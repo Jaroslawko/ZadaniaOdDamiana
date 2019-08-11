e2e1e05c9d59071375741ca009d7091fd6df51ad
Jarek Kobier <kobier4@gazeta.pl>
2019-08-11 22:29:33 +0200

    skrypt który czyta z pliku i pokazuje gdzie szuka pliku tekstowego

---
+++ src/JavaStartIO/JavaStartIO.java
@@ -0,0 +1,25 @@
+package JavaStartIO;
+
+
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class JavaStartIO {
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        File plik = new File("jarek.txt");
+
+        Scanner in = new Scanner(plik);
+        String zdanie = in.nextLine();
+        System.out.println(zdanie);
+
+        boolean exists = plik.exists();
+        System.out.println("Czy istnieje: " + exists);
+        String absolutePath = plik.getAbsolutePath();
+        System.out.println("Ten plik istnieje w " + absolutePath);
+
+    }
+}
